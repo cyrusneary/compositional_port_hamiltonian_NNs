58b90d6a1cfb82847e1ed4b9e2ed2335878919f0
Cyrus Neary <cyrusneary@gmail.com>
2022-09-13 14:23:00 -0500

    Changing import locations to avoid circular import dependencies.

--- helpers/model_factories.py
+++ helpers/model_factories.py
@@ -4,13 +4,6 @@ from abc import abstractmethod
 import sys
 sys.path.append('..')
 
-from models.mlp import MLP
-from models.neural_ode import NODE
-from models.hamiltonian_neural_ode import HNODE
-from models.ph_node import PHNODE
-from models.mlp_autoencoder import MlpAutoencoder
-from models.autoencoder_node import AutoencoderNODE
-
 import jax
 
 class ModelFactory():
@@ -29,8 +22,9 @@ class ModelFactory():
 class NodeFactory(ModelFactory):
     """Factory that creates a vanilla neural ODE."""
 
-    def create_model(self, seed) -> NODE:
+    def create_model(self, seed):
         """Instantiate a vanilla neural ODE."""
+        from models.neural_ode import NODE
         return NODE(rng_key=jax.random.PRNGKey(seed),
                     input_dim=self.model_setup['input_dim'],
                     output_dim=self.model_setup['output_dim'],
@@ -40,7 +34,8 @@ class NodeFactory(ModelFactory):
 class HamiltonianNodeFactory(ModelFactory):
     """Factory that creates a Hamiltonian neural ODE."""
 
-    def create_model(self, seed) -> HNODE:
+    def create_model(self, seed):
+        from models.hamiltonian_neural_ode import HNODE
         """Instantiate a hamiltonian neural ODE."""
         return HNODE(rng_key=jax.random.PRNGKey(seed),
                         input_dim=self.model_setup['input_dim'],
@@ -51,7 +46,8 @@ class HamiltonianNodeFactory(ModelFactory):
 class MlpFactory(ModelFactory):
     """Factory that creates a multi-layer perceptron."""
 
-    def create_model(self, seed) -> MLP:
+    def create_model(self, seed):
+        from models.mlp import MLP
         """Instantiate a multi-layer perceptron."""
         return MLP(rng_key=jax.random.PRNGKey(seed),
                 input_dim=self.model_setup['input_dim'],
@@ -59,19 +55,33 @@ class MlpFactory(ModelFactory):
                 nn_setup_params=self.model_setup['nn_setup_params'])
 
 class PortHamiltonianNodeFactory(ModelFactory):
+    """Factory that creates a compositional port-Hamiltonian neural ODE."""
+
+    def create_model(self, seed):
+        from models.ph_node import PHNODE
+        """Instantiate a compositional port-Hamiltonian neural ODE."""
+        return PHNODE(rng_key=jax.random.PRNGKey(seed),
+                        input_dim=self.model_setup['input_dim'],
+                        output_dim=self.model_setup['output_dim'],
+                        dt=self.model_setup['dt'],
+                        nn_setup_params=self.model_setup['nn_setup_params'])
+
+class CompositionalPortHamiltonianNodeFactory(ModelFactory):
     """Factory that creates a port-Hamiltonian nerual ODE."""
     
-    def create_model(self, seed) -> PHNODE:
+    def create_model(self, seed):
         """Instantiate a port-Hamiltonian neural ODE."""
-        return PHNODE(rng_key=jax.random.PRNGKey(seed),
+        from models.compositional_ph_node import CompositionalPHNODE
+        return CompositionalPHNODE(rng_key=jax.random.PRNGKey(seed),
                         dt=self.model_setup['dt'],
                         model_setup=self.model_setup)
 
 class AutoencoderFactory(ModelFactory):
     """Factory that creates an autoencoder."""
 
-    def create_model(self, seed) -> MlpAutoencoder:
+    def create_model(self, seed):
         """Instantiate an autoencoder."""
+        from models.mlp_autoencoder import MlpAutoencoder
         return MlpAutoencoder(rng_key=jax.random.PRNGKey(seed),
                                 input_dim=self.model_setup['input_dim'],
                                 latent_dim=self.model_setup['latent_dim'],
@@ -82,7 +92,8 @@ class AutoencoderFactory(ModelFactory):
 class AutoencoderNodeFactory(ModelFactory):
     """Factory that creates an autoencoder Neural ODE."""
 
-    def create_model(self, seed) -> AutoencoderNODE:
+    def create_model(self, seed):
+        from models.autoencoder_node import AutoencoderNODE
         return AutoencoderNODE(
             rng_key=jax.random.PRNGKey(seed),
             input_dim=self.model_setup['input_dim'],
@@ -99,21 +110,12 @@ model_factories = {
     'node' : NodeFactory,
     'hnode' : HamiltonianNodeFactory,
     'mlp' : MlpFactory,
-    'phnode' : PortHamiltonianNodeFactory,
+    'compositional_phnode' : CompositionalPortHamiltonianNodeFactory,
     'autoencoder_mlp' : AutoencoderFactory,
     'autoencoder_node' : AutoencoderNodeFactory,
+    'phnode' : PortHamiltonianNodeFactory,
 }
 
 def get_model_factory(model_setup):
     factory_name = model_setup['model_type']
     return model_factories[factory_name](model_setup)
-
-def choose_nonlinearity(nonlinearity):
-    if nonlinearity == 'relu':
-        return jax.nn.relu
-    elif nonlinearity == 'tanh':
-        return jax.nn.tanh
-    elif nonlinearity == 'leaky_relu':
-        return jax.nn.leaky_relu
-    else:
-        raise ValueError('Unknown nonlinearity: {}'.format(nonlinearity))
