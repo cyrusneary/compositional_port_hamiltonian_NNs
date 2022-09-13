b9fbfea2bd4d68be98118f81127409ba515c0054
Cyrus Neary <cyrusneary@gmail.com>
2022-09-13 17:08:31 -0500

    Updating plotting code.

--- plotting/plot_double_spring_mass_composite_ph_node.py
+++ plotting/plot_double_spring_mass_composite_ph_node.py
@@ -9,8 +9,16 @@ import jax
 from environments.ph_system import PHSystem
 
 from common import load_config_file, load_dataset, load_model, load_metrics
+import argparse
 
-sacred_run_index = 98
+# sacred_run_index = 98
+
+parser = argparse.ArgumentParser(description='Plot the training results of the \
+    Sacred experiment specified by the provided index.')
+parser.add_argument("-r", "--run_index", default=1, help="The index of the Sacred run to load.")
+args = parser.parse_args()
+
+sacred_run_index = args.run_index
 sacred_save_path = os.path.abspath('../experiments/sacred_runs/')
 
 config = load_config_file(sacred_run_index, sacred_save_path)
