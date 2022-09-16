5e8674ee9ce2ac092ecae66aea84ab8a5722f3bd
Cyrus Neary <cyrusneary@gmail.com>
2022-09-15 21:03:46 -0500

    Simplifying plotting code.

--- plotting/plot_training_results.py
+++ plotting/plot_training_results.py
@@ -18,31 +18,18 @@ sacred_save_path = os.path.abspath('../experiments/sacred_runs/')
 
 results = load_metrics(sacred_run_index, sacred_save_path)
 
-# If there are only one list of training loss values and one list of testing 
-# loss values, plot them each.
-if len(results)  == 2:
-    fig = plt.figure(figsize=(5,5))
-    ax = fig.add_subplot(111)
-    ax.plot(results['training.loss']['steps'], results['training.loss']['values'], color='blue')
-    ax.plot(results['training.loss']['steps'], results['testing.loss']['values'], color='red')
-    ax.set_yscale('log')
-    ax.grid()
-    plt.show()
-
-# Otherwise, there are separate lists of training losses for each of the
-# submodels. Plot these training losses separately.
-else:
-    fig = plt.figure(figsize=(5,5))
-    ax = fig.add_subplot(111)
-    for key in results.keys():
-        if key == 'testing.loss': continue # Plot the testing loss last.
-        ax.plot(results[key]['steps'], results[key]['values'], label=key)
-    ax.plot(
-        results['testing.loss']['steps'], 
-        results['testing.loss']['values'], 
-        label='testing.loss', 
-    )
-    ax.set_yscale('log')
-    ax.grid()
-    ax.legend()
-    plt.show()
+# Plot the training results
+fig = plt.figure(figsize=(5,5))
+ax = fig.add_subplot(111)
+for key in results.keys():
+    if key == 'testing.loss': continue # Plot the testing loss last.
+    ax.plot(results[key]['steps'], results[key]['values'], label=key)
+ax.plot(
+    results['testing.total_loss']['steps'], 
+    results['testing.total_loss']['values'], 
+    label='testing.total_loss', 
+)
+ax.set_yscale('log')
+ax.grid()
+ax.legend()
+plt.show()
